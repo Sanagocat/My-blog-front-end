30a7ef47e05e1dd438802c73f63ec3057396b6f3
Sanagocat <105789022+Sanagocat@users.noreply.github.com>
2025-04-13 21:14:00 +0700

    Delete DanwooBlogFrontend directory

--- DanwooBlogFrontend/complete.js
+++
@@ -1,7 +0,0 @@
-const okButton =document.getElementById('OK');
-okButton.addEventListener('click', okFunction);
-
-function okFunction()
-{
-    window.location.href = 'index.html'; // write.html 페이지로 이동
-}
